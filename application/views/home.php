3f9afc720def8d6641fc81cf3b520549911493a6
coffeejack <ubcvincentttsui@gmail.com>
2013-06-22 15:22:22 -0700

    coming soon

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,36 @@
-HELLO WORLD
+<!-- css -->
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+
+<!-- js -->
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+
+<head>
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+      <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="brand" href="./index.php">Vince Tsui</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+          <li class="">
+            <a href="#">Home</a>
+          </li>
+          <li class="">
+            <a href="#">Projects</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+</head>
+<body>
+	<img src="http://kaltrading.com/newsite/wp-content/uploads/2013/03/red_coming_soon_stamp.png" style="margin-top: 10%;display: block;
+margin-left: auto;
+margin-right: auto;">
+</body>
